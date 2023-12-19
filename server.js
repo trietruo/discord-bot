968e0c75f5d29d935882859d80cb6f893038d0bc
trietruo <151657157+trietruo@users.noreply.github.com>
2023-12-19 18:15:22 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const server = express();
+
+server.all(`/`, (req, res) => {
+	res.send(`Result: OK.`);
+});
+
+function keepAlive() {
+	server.listen(3000, () => {
+		console.log(`server.js --> Ready!`);
+	});
+}
+
+module.exports = keepAlive;
