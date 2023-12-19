10e2eea64094d111833c976c9be0b1c65db48cb9
trietruo <151657157+trietruo@users.noreply.github.com>
2023-12-19 18:19:11 -0500

    Create dbInit.js

---
+++ dbInit.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('database', 'username', 'password', {
+	host: 'localhost',
+	dialect: 'sqlite',
+	logging: false,
+	storage: 'database.sqlite',
+});
+
+const CurrencyShop = require('./models/CurrencyShop.js')(sequelize, Sequelize.DataTypes);
+require('./models/Users.js')(sequelize, Sequelize.DataTypes);
+require('./models/UserItems.js')(sequelize, Sequelize.DataTypes);
+
+const force = process.argv.includes('--force') || process.argv.includes('-f');
+
+sequelize.sync({ force }).then(async () => {
+	const shop = [
+		CurrencyShop.upsert({ name: 'Orange Blood', cost: 5 }),
+		CurrencyShop.upsert({ name: 'Drama', cost: 3 }),
+	];
+
+	await Promise.all(shop);
+	console.log('Database synced');
+
+	sequelize.close();
+}).catch(console.error);
