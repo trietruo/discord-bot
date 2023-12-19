3c6764f9e571b623f4eb2408be0da80b4b4718bd
trietruo <151657157+trietruo@users.noreply.github.com>
2023-12-19 18:25:00 -0500

    Create Users.js

---
+++ models/Users.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+	return sequelize.define('users', {
+		user_id: {
+			type: DataTypes.STRING,
+			primaryKey: true,
+		},
+		balance: {
+			type: DataTypes.INTEGER,
+			defaultValue: 0,
+			allowNull: false,
+		},
+	}, {
+		// Disables the createdAt and updatedAt columns
+		timestamps: false,
+	});
+};
